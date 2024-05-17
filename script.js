dcff42ab42abb9e69d3f313410e5a553c3247a2f
vinicius <mvs_tj14@hotmail.com>
2024-05-16 22:11:38 -0300

    conflite resolve

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 
 templateGenerate(getArrayList());
-<<<<<<< HEAD
-=======
-
-let btnAdc = getElement('#btnAdicionar');
->>>>>>> 47f9f08c6f3d0a9b6a2bbb1f556410408983216f
 
 listenerEvent('#inputText', 'input', (event) => {
   onlyString(event);
@@ -21,11 +16,8 @@ listenerEvent('#inputEdit', 'input', (event) => {
 
 listenerEvent('#btn-search', 'click', () => {
   toogleClass('#inputSearch', 'show');
-<<<<<<< HEAD
   clearCamp(getElement('#inputSearch'));
   templateGenerate(getArrayList());
-=======
->>>>>>> 47f9f08c6f3d0a9b6a2bbb1f556410408983216f
 });
 
 listenerEvent('#btnAdicionar', 'click', () => {
